58734aefe49a2249eb691d33683903986193c8f6
Matthew <kohney319m@gmail.com>
2021-10-13 12:59:44 -0600

    commit tree 77c806ecbd2dd4632a9593461fd448e5cc4d26a8

--- index.js
+++ index.js
@@ -1,4 +1,79 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(records) {
+    let employeeRecords = [];
+    // Converts each nested Array into an employee record using createEmployeeRecord and accumulates it to a new array
+    records.map(record => {
+        let recorded = createEmployeeRecord(record);
+        employeeRecords.push(recorded);
+    })
+    // returns an array of objects
+    return employeeRecords;
+}
+
+function createTimeInEvent(dateStamp) {
+    // Add an Object with keys type, hour and date
+    const splitDateStamp = dateStamp.split(" ");
+    const date = splitDateStamp[0];
+    const hour = splitDateStamp[1];
+    const timeInObj = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date,
+    }
+    this.timeInEvents.push(timeInObj);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    // Add an Object with keys type, hour and date
+    const splitDateStamp = dateStamp.split(" ");
+    const date = splitDateStamp[0];
+    const hour = splitDateStamp[1];
+    const timeOutObj = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date,
+    }
+    this.timeOutEvents.push(timeOutObj);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    // Given a date, find the number of hours elapsed between that date's timeInEvent and timeOutEvent
+    // Find & store the timeIn from the date passed
+    let timeIn;
+    this.timeInEvents.find(el => {
+        if (date === el.date) {
+            return timeIn = el.hour / 100;
+        }
+    });
+    let timeOut;
+    this.timeOutEvents.find(el => {
+        if (date === el.date) {
+            return timeOut = el.hour / 100;
+        }
+    });
+    return timeOut - timeIn
+}
+
+function wagesEarnedOnDate(date) {
+    // Using hoursWorkedOnDate, multiply the hours by the record's payRate to determine amount owed. Amount should be returned as a number.
+    const hours = hoursWorkedOnDate.call(this, date);
+    const wage = this.payPerHour;
+    // return pay owed
+    return hours * wage;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +96,16 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(collection, firstNameString) {
+    // Test the firstName field for a match with the firstName argument
+    return collection.find(rec => {
+        return rec.firstName === firstNameString
+    })
+}
+
+
+function calculatePayroll(collection) {
+    return collection.reduce((e, record) => {
+        return e + allWagesFor.call(record);
+    }, 0)
+}
